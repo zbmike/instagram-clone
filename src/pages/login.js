46d0fae236eb6749fefdd3d19108a7add77cbb0e
Zhiyuan He <zh2312@columbia.edu>
2021-04-26 11:06:40 -0400

    First taste of tailwind

--- src/pages/login.js
+++ src/pages/login.js
@@ -19,7 +19,12 @@ export default function App() {
 
     return (
         <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-            Login page
+            <div className="flex w-3/5">
+                <img src="/images/iphone-with-profile.jpg" alt="iPhone with Instagram app" />
+            </div>
+            <div className="flex flex-col w-2/5">
+                <p>form here</p>
+            </div>
         </div>
     );
 }
