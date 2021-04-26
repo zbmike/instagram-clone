738c612791d89fc6245c6f61c674e7623f08f95e
Zhiyuan He <zh2312@columbia.edu>
2021-04-26 13:31:53 -0400

    Create files for the dashboard

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import * as ROUTES from './constants/routes';
 
 const Login = lazy(() => import('./pages/login'));
 const SignUp = lazy(() => import('./pages/signup'));
+const Dashboard = lazy(() => import('./pages/dashboard'));
 const NotFound = lazy(() => import('./pages/not-found'));
 
 function App() {
@@ -12,8 +13,9 @@ function App() {
       <Suspense fallback={<p>Loading ...</p>} >
       <Switch>
         
-        <Route path={ROUTES.LOGIN} component={Login} exact/>
-        <Route path={ROUTES.SIGN_UP} component={SignUp} exact/>
+        <Route path={ROUTES.LOGIN} component={Login}/>
+        <Route path={ROUTES.SIGN_UP} component={SignUp}/>
+        <Route path={ROUTES.DASHBOARD} component={Dashboard}/>
         <Route component={NotFound} />
       </Switch>
       </Suspense>
