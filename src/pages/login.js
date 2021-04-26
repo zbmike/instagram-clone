55c4f9b166e3f037b7928aca844debcf6110bac0
Zhiyuan He <zh2312@columbia.edu>
2021-04-26 11:50:16 -0400

    Finish simple login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,31 +1,77 @@
 import React, { useContext, useEffect, useState } from "react";
-import { useHistory } from 'react-router-dom';
-import FirebaseContext from '../context/firebase';
+import { Link, useHistory } from "react-router-dom";
+import FirebaseContext from "../context/firebase";
 
 export default function App() {
-    const history = useHistory();
-    const { firebase } = useContext(FirebaseContext);
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
 
-    const [emailAddress, setEmailAdress] = useState('');
-    const [password, setPassword] = useState('');
+  const [emailAddress, setEmailAdress] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [error, setError] = useState('');
-    const isInvalid = password === '' || emailAddress === '';
-    const handleLogin = () => {};
+  const [error, setError] = useState("");
+  const isInvalid = password === "" || emailAddress === "";
+  const handleLogin = () => {};
 
-    useEffect(() => {
-        document.title = 'Login - Instagram';
-    })
+  useEffect(() => {
+    document.title = "Login - Instagram";
+  });
 
-    return (
-        <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-            <div className="flex w-3/5">
-                <img src="/images/iphone-with-profile.jpg" alt="iPhone with Instagram app" />
-            </div>
-            <div className="flex flex-col w-2/5">
-                <p>form here</p>
-            </div>
+  return (
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+      <div className="flex w-3/5">
+        <img
+          src="/images/iphone-with-profile.jpg"
+          alt="iPhone with Instagram app"
+        />
+      </div>
+      <div className="flex flex-col w-2/5">
+        <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
+          <h1 className="flex justify-center  w-full">
+            <img
+              src="/images/logo.png"
+              alt="Instagram"
+              className="mt-2 w-6/12 mb-4"
+            />
+          </h1>
+
+          {error && <p className="mb-4 text-cs text-red-primary">{error}</p>}
+
+          <form onSubmit={handleLogin} method="POST">
+            <input
+              aria-label="Enter your email address"
+              type="text"
+              placeholder="Email address"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setEmailAdress(target.value)}
+            />
+            <input
+              aria-label="Enter your password"
+              type="password"
+              placeholder="Password"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <button
+              disabled={isInvalid}
+              type="submit"
+              className={`bg-blue-medium text-white w-full rounded h-8 font-bold ${
+                isInvalid && "opacity-50"
+              }`}
+            >
+              Login
+            </button>
+          </form>
         </div>
-    );
+        <div className="flex justify-center items-center flex-col w-full bg-white p-4 border border-gray-primary rounded">
+          <p className="text-sm">
+            Don&apos;t have an account?{`  `}
+            <Link to="/signup" className="font-bold text-blue-medium">
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
-  
