9a7d4e7d57792881e4502214ddbc87c0ea24ee3c
Zhiyuan He <zh2312@columbia.edu>
2021-04-27 14:16:21 -0400

    Add image for post component

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,11 +1,13 @@
 import React, { useRef } from "react";
 import PropTypes from "prop-types";
 import Header from "./header";
+import Image from "./image";
 
 export default function Post({ content }) {
   return (
-    <div className="rounded col-span-4 border bg-white border-gray-primary mb-16">
+    <div className="rounded col-span-4 border bg-white border-gray-primary mb-8">
       <Header username={content.username} />
+      <Image src={content.imageSrc} caption={content.caption} />
     </div>
   );
 }
