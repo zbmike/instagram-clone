8eb9d2fa8d806d387d62a1807adec784545db4b2
Zhiyuan He <zh2312@columbia.edu>
2021-04-27 14:12:24 -0400

    Add header for post component

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,20 +1,24 @@
-import React, { useRef } from 'react';
-import PropTypes from 'prop-types';
+import React, { useRef } from "react";
+import PropTypes from "prop-types";
+import Header from "./header";
 
-export default function Post({content}) {
-    console.log(content);
-    return <p>post</p>;
+export default function Post({ content }) {
+  return (
+    <div className="rounded col-span-4 border bg-white border-gray-primary mb-16">
+      <Header username={content.username} />
+    </div>
+  );
 }
 
 Post.propTypes = {
-    content: PropTypes.shape({
-        username: PropTypes.string,
-        imageSrc: PropTypes.string,
-        caption: PropTypes.string,
-        docId: PropTypes.string,
-        userLikedPhoto: PropTypes.bool,
-        likes: PropTypes.array,
-        comments: PropTypes.array,
-        dateCreated: PropTypes.number,
-    })
-}
+  content: PropTypes.shape({
+    username: PropTypes.string,
+    imageSrc: PropTypes.string,
+    caption: PropTypes.string,
+    docId: PropTypes.string,
+    userLikedPhoto: PropTypes.bool,
+    likes: PropTypes.array,
+    comments: PropTypes.array,
+    dateCreated: PropTypes.number,
+  }),
+};
