e902912de79f6572ea0c1340960043c1f641bc5d
Zhiyuan He <zh2312@columbia.edu>
2021-04-28 21:46:39 -0400

    Add purge for tailwind for optimizing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,34 @@
 module.exports = {
-    purge: [],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-      fill : (theme) => ({
-        red: theme('colors.red.primary')
-      }),
-      colors: {
-        white: '#ffffff',
-        blue: {
-          medium: '#005c98'
-        },
-        black: {
-          light: '#262626',
-          faded: '#00000059'
-        },
-        gray: {
-          base:'#616161',
-          background: 'fafafa',
-          primary: '#dbdbdb',
-        },
-        red: {
-          primary: '#ed4956'
-        }
+  purge: {
+    content: ["./src/**/*.js", "./src/**/**/*.js"],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fill: (theme) => ({
+      red: theme("colors.red.primary"),
+    }),
+    colors: {
+      white: "#ffffff",
+      blue: {
+        medium: "#005c98",
       },
-      extend: {},
+      black: {
+        light: "#262626",
+        faded: "#00000059",
+      },
+      gray: {
+        base: "#616161",
+        background: "fafafa",
+        primary: "#dbdbdb",
+      },
+      red: {
+        primary: "#ed4956",
+      },
     },
-    variants: {
-      display: ['group-hover']
-    },
-    plugins: [],
-  }
+    extend: {},
+  },
+  variants: {
+    display: ["group-hover"],
+  },
+  plugins: [],
+};
