fb3bebf92d1f3d2e92edeb7938b5a99cb8dba64c
Zhiyuan He <zh2312@columbia.edu>
2021-04-28 21:35:15 -0400

    FIx issue in suggestion component

--- src/components/sidebar/suggestion.js
+++ src/components/sidebar/suggestion.js
@@ -1,7 +1,6 @@
 import PropTypes from "prop-types";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Skeleton from "react-loading-skeleton";
-import { useEffect } from "react/cjs/react.development";
 
 import SuggestedProfiles from './suggested-profile';
 import { getSuggestedProfiles } from "../../services/firebase";
@@ -23,10 +22,10 @@ export default function Suggestion({ userId, following, loggedInUserDocId }) {
     <Skeleton count={1} height={150} className="mt-5" />
   ) : profiles.length > 0 ? (
     <div className="rounded flex flex-col">
-      <div className="text-sm flex items-center align items justify-between mb-2">
-        <p className="font-bold text-gray-base">Suggestion for you</p>
+      <div className="text-sm flex items-center align-items justify-between mb-2">
+        <p className="font-bold text-gray-base">Suggestions for you</p>
       </div>
-      <div className="mt-4 grid gap 5">
+      <div className="mt-4 grid gap-5">
         {profiles.map((profile) => (
           <SuggestedProfiles
             key={profile.docId}
